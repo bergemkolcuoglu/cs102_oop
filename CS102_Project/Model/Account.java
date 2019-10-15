c4369951bf65bca243f4364fbeec04fb9d5be257
Bergem Kolcuoğlu <43249333+bergemkolcuoglu@users.noreply.github.com>
2019-10-15 19:22:16 +0300

    Add files via upload

---
+++ CS102_Project/Model/Account.java
@@ -0,0 +1,22 @@
+package Model;
+
+
+public class Account {
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void increaseAccount(double amount) {
+        balance+=amount;
+    }
+
+    public void decreaseAccount(double amount) {
+        balance-=amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ CS102_Project/Model/Main.java
@@ -0,0 +1,28 @@
+package Model;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        RetailManagement retailManagement = new RetailManagement(new Account(100));
+
+
+        Warehouse warehouse = new Warehouse("Bergem", new Account(1));
+
+        Location location = new Location("istanbul","10" );
+
+        Retailer retailer = new Retailer("Eren",location,new Account(1000));
+
+
+        warehouse.addLocation(location);
+
+
+        retailManagement.addWarehouse(warehouse);
+
+        retailManagement.addRetailer(retailer);
+
+        System.out.println(warehouse.getName());
+
+
+    }
+}
